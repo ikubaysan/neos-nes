35c4506042046c45b1bbe13f8e1b53cd668667bc
ikubay <ikubaysan@gmail.com>
2023-05-21 18:56:25 -0700

    Remove process_array()

--- ctypes_test/ctypes_test.py
+++ ctypes_test/ctypes_test.py
@@ -20,11 +20,6 @@ def create_and_process_array():
     # Load the shared library
     mylib = ctypes.CDLL('./mylib.so', winmode=0)
 
-    # Define the function prototype
-    process_array = mylib.process_array
-    process_array.argtypes = [ctypes.POINTER(Array3D)]
-    process_array.restype = ctypes.c_char_p
-
     frame_to_string = mylib.frame_to_string
     frame_to_string.argtypes = [ctypes.POINTER(Array3D), ctypes.POINTER(BoolArray)]
     frame_to_string.restype = ctypes.c_char_p
@@ -37,15 +32,6 @@ def create_and_process_array():
 
     # Convert the ndarray to a C-compatible structure
     array = Array3D((state.shape[0], state.shape[1], state.shape[2]), state.ctypes.data_as(ctypes.POINTER(ctypes.c_ubyte)))
-
-    # Call the C++ function
-    result = process_array(ctypes.byref(array))
-
-    # Convert the returned value to a Python string
-    string_result = result.decode()
-
-    # Print the string
-    print(string_result)
 
     # Call the C++ frame_to_string function
     changed_pixels = BoolArray(state.shape[0] * state.shape[1], (ctypes.c_bool * (state.shape[0] * state.shape[1]))(*[True]*state.shape[0]*state.shape[1]))
