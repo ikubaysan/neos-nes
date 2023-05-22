ee38a71831fc2a71f6da7838bcbb3d337eaf0bf0
ikubay <ikubaysan@gmail.com>
2023-05-21 18:15:27 -0700

    cout looks fine, but decoding in python is not working

--- ctypes_test/ctypes_test.py
+++ ctypes_test/ctypes_test.py
@@ -1,31 +1,62 @@
 import numpy as np
 import ctypes
 
-# Load the shared library
-mylib = ctypes.CDLL('./mylib.so', winmode=0)
 
-# Define the function prototype
 class Array3D(ctypes.Structure):
     _fields_ = [
         ("shape", ctypes.c_int * 3),
         ("data", ctypes.POINTER(ctypes.c_ubyte)),
     ]
 
-process_array = mylib.process_array
-process_array.argtypes = [ctypes.POINTER(Array3D)]
-process_array.restype = ctypes.c_char_p
 
-# Create a 3D ndarray
-state = np.random.randint(0, 256, (240, 256, 3), dtype=np.uint8)
+class BoolArray(ctypes.Structure):
+    _fields_ = [
+        ("size", ctypes.c_int),
+        ("data", ctypes.POINTER(ctypes.c_bool)),
+    ]
 
-# Convert the ndarray to a C-compatible structure
-array = Array3D((state.shape[0], state.shape[1], state.shape[2]), state.ctypes.data_as(ctypes.POINTER(ctypes.c_ubyte)))
 
-# Call the C++ function
-result = process_array(ctypes.byref(array))
+def create_and_process_array():
+    # Load the shared library
+    mylib = ctypes.CDLL('./mylib.so', winmode=0)
 
-# Convert the returned value to a Python string
-string_result = result.decode()
+    # Define the function prototype
+    process_array = mylib.process_array
+    process_array.argtypes = [ctypes.POINTER(Array3D)]
+    process_array.restype = ctypes.c_char_p
 
-# Print the string
-print(string_result)
+    frame_to_string = mylib.frame_to_string
+    frame_to_string.argtypes = [ctypes.POINTER(Array3D), ctypes.POINTER(BoolArray)]
+    frame_to_string.restype = ctypes.c_char_p
+
+    # Create a 3D ndarray
+    state = np.random.randint(0, 256, (240, 256, 3), dtype=np.uint8)
+
+    # Make all values multiples of 4
+    state = np.floor_divide(state, 4) * 4
+
+    # Convert the ndarray to a C-compatible structure
+    array = Array3D((state.shape[0], state.shape[1], state.shape[2]), state.ctypes.data_as(ctypes.POINTER(ctypes.c_ubyte)))
+
+    # Call the C++ function
+    result = process_array(ctypes.byref(array))
+
+    # Convert the returned value to a Python string
+    string_result = result.decode()
+
+    # Print the string
+    print(string_result)
+
+    # Call the C++ frame_to_string function
+    changed_pixels = BoolArray(state.shape[0] * state.shape[1], (ctypes.c_bool * (state.shape[0] * state.shape[1]))(*[True]*state.shape[0]*state.shape[1]))
+    result = frame_to_string(ctypes.byref(array), ctypes.byref(changed_pixels))
+
+    # Convert the returned value to a Python string
+    string_result = result.decode()
+
+    # Print the string
+    print(string_result)
+
+
+if __name__ == "__main__":
+    create_and_process_array()
--- ctypes_test/mylib.cpp
+++ ctypes_test/mylib.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <sstream>
+#include <iostream>
 
 extern "C" {
 
@@ -7,6 +8,65 @@ extern "C" {
         int shape[3];
         unsigned char* data;
     } Array3D;
+
+    typedef struct {
+        int size;
+        bool* data;
+    } BoolArray;
+
+    const char* frame_to_string(Array3D* array, BoolArray* changed_pixels) {
+        std::stringstream ss;
+        std::string last_color = "";
+        int same_color_start = 0;
+        int total_pixels = array->shape[0] * array->shape[1];
+        bool changed;
+
+        for (int i = 0; i < total_pixels; ++i) {
+            unsigned char* pixel = array->data + i * array->shape[2];
+            int r = pixel[0] >> 2;
+            int g = pixel[1] >> 2;
+            int b = pixel[2] >> 2;
+
+            int rgb_int = b << 10 | g << 5 | r;  // Swap red and blue channels
+
+            // Adjust if in the Unicode surrogate range
+            if (0xD800 <= rgb_int && rgb_int <= 0xDFFF) {
+                if (rgb_int < 0xDC00)
+                    rgb_int = 0xD7FF;  // Maximum value just before the surrogate range
+                else
+                    rgb_int = 0xE000;  // Minimum value just after the surrogate range
+            }
+
+            std::string color(1, rgb_int);
+
+            // Check if pixel has changed
+            if (changed_pixels == nullptr)
+                changed = true;
+            else
+                changed = changed_pixels->data[i];
+
+            if (changed && color != last_color) {
+                if (!last_color.empty() && same_color_start >= 0) {
+                    ss << same_color_start << "+" << i - 1 - same_color_start << "_" << last_color;
+                }
+                same_color_start = i;
+                last_color = color;
+            } else if (!changed && same_color_start >= 0) {
+                ss << same_color_start << "+" << i - 1 - same_color_start << "_" << last_color;
+                same_color_start = -1;
+                last_color = "";
+            }
+
+            if (i == total_pixels - 1 && same_color_start >= 0) {  // the end of the pixels, add the last color
+                ss << same_color_start << "+" << i - same_color_start << "_" << last_color;
+            }
+        }
+
+        static std::string s = ss.str();
+        std::cout << s << std::endl;
+        return s.c_str();
+    }
+
 
     const char* process_array(Array3D* array) {
         std::stringstream ss;
