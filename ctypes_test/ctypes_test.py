87663ded23642cee1b8569e62510b3899798d2ad
ikubay <ikubaysan@gmail.com>
2023-05-21 17:49:42 -0700

    Add sample  ctypes 3d np array processing

--- ctypes_test/ctypes_test.py
+++ ctypes_test/ctypes_test.py
@@ -1,14 +1,28 @@
+import numpy as np
 import ctypes
 
 # Load the shared library
 mylib = ctypes.CDLL('./mylib.so', winmode=0)
 
 # Define the function prototype
-get_string = mylib.get_string
-get_string.restype = ctypes.c_char_p
+class Array3D(ctypes.Structure):
+    _fields_ = [
+        ("shape", ctypes.c_int * 3),
+        ("data", ctypes.POINTER(ctypes.c_ubyte)),
+    ]
+
+process_array = mylib.process_array
+process_array.argtypes = [ctypes.POINTER(Array3D)]
+process_array.restype = ctypes.c_char_p
+
+# Create a 3D ndarray
+state = np.random.randint(0, 256, (240, 256, 3), dtype=np.uint8)
+
+# Convert the ndarray to a C-compatible structure
+array = Array3D((state.shape[0], state.shape[1], state.shape[2]), state.ctypes.data_as(ctypes.POINTER(ctypes.c_ubyte)))
 
 # Call the C++ function
-result = get_string()
+result = process_array(ctypes.byref(array))
 
 # Convert the returned value to a Python string
 string_result = result.decode()
--- ctypes_test/mylib.cpp
+++ ctypes_test/mylib.cpp
@@ -1,6 +1,27 @@
 #include <string>
+#include <sstream>
 
-extern "C" const char* get_string() {
-    std::string str = "Hello from C++";
-    return str.c_str();
+extern "C" {
+
+    typedef struct {
+        int shape[3];
+        unsigned char* data;
+    } Array3D;
+
+    const char* process_array(Array3D* array) {
+        std::stringstream ss;
+
+        for (int i = 0; i < array->shape[0]; ++i) {
+            unsigned char* row = array->data + i * array->shape[1] * array->shape[2];
+            ss << (int)row[1 * array->shape[2] + 0] << ",";  // R
+            ss << (int)row[1 * array->shape[2] + 1] << ",";  // G
+            ss << (int)row[1 * array->shape[2] + 2] << "\n"; // B
+
+            if (i < array->shape[0] - 1)
+                ss << ",";
+        }
+
+        static std::string s = ss.str();
+        return s.c_str();
+    }
 }
