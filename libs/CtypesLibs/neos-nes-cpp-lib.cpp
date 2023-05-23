a581e4edc46c5cd82d8419aaeac6c74723838302
ikubay <ikubaysan@gmail.com>
2023-05-22 20:28:43 -0700

    Do not use stringstreams

--- libs/CtypesLibs/neos-nes-cpp-lib.cpp
+++ libs/CtypesLibs/neos-nes-cpp-lib.cpp
@@ -46,84 +46,61 @@ extern "C" {
 
     // Function to convert the array of pixels to a string representation
     void frame_to_string(Array3D* array, BoolArray* changed_pixels, char* output) {
-        std::stringstream ss;
+        std::string s;
         std::string last_color = "";
         int same_color_start = -1;
         int total_pixels = array->shape[0] * array->shape[1];
         bool changed;
 
-        // Iterate over each pixel in the array
         for (int i = 0; i < total_pixels; ++i) {
-            // Check if the pixel has changed
             if (changed_pixels == nullptr)
                 changed = true;
             else
                 changed = changed_pixels->data[i];
 
             if (changed) {
-                // passed_in_change_count++;
-
-                // Access the RGB values of the pixel
                 unsigned char* pixel = array->data + i * array->shape[2];
                 int r = pixel[0] >> 2;
                 int g = pixel[1] >> 2;
                 int b = pixel[2] >> 2;
+                int rgb_int = b << 10 | g << 5 | r;
 
-                // Combine RGB values to obtain an integer representation of the color
-                int rgb_int = b << 10 | g << 5 | r;  // Swap red and blue channels
-
-                // Adjust if in the Unicode surrogate range
                 if (0xD800 <= rgb_int && rgb_int <= 0xDFFF) {
                     if (rgb_int < 0xDC00)
-                        rgb_int = 0xD7FF;  // Maximum value just before the surrogate range
+                        rgb_int = 0xD7FF;
                     else
-                        rgb_int = 0xE000;  // Minimum value just after the surrogate range
+                        rgb_int = 0xE000;
                 }
 
                 auto cached = rgb_to_utf8_cache.find(rgb_int);
                 std::string color;
                 if (cached != rgb_to_utf8_cache.end()) {
-                    // If found in the cache, use the cached value
                     color = cached->second;
                 } else {
-                    // If not found in the cache, compute the value and store it in the cache
                     color = encode_utf8(rgb_int);
                     rgb_to_utf8_cache[rgb_int] = color;
                 }
 
-                if (color != last_color)
-                {
+                if (color != last_color) {
                     if (!last_color.empty() && same_color_start != -1) {
-                    ss << same_color_start << "+" << i - 1 - same_color_start << "_" << last_color;
-                    //std::cout << "Found t1 Range: " << same_color_start << "+" << i - 1 - same_color_start << "_" << last_color << std::endl;
+                        s += std::to_string(same_color_start) + "+" + std::to_string(i - 1 - same_color_start) + "_" + last_color;
                     }
                     same_color_start = i;
                     last_color = color;
                 }
-         
-            } else if (!changed && same_color_start != -1) {
-                ss << same_color_start << "+" << i - 1 - same_color_start << "_" << last_color;
-                //std::cout << "Found t2 range: " << same_color_start << "+" << i - 1 - same_color_start << "_" << last_color << std::endl;
+            } else if (same_color_start != -1 && !changed) {
+                s += std::to_string(same_color_start) + "+" + std::to_string(i - 1 - same_color_start) + "_" + last_color;
                 same_color_start = -1;
                 last_color = "";
             }
         }
 
-        // After all pixels have been processed, handle any remaining color streak
         if (same_color_start != -1) {
-            ss << same_color_start << "+" << total_pixels - 1 - same_color_start << "_" << last_color;
-            //std::cout << "Found Range: " << same_color_start << "+" << total_pixels - 1 - same_color_start << "_" << last_color << std::endl;
+            s += std::to_string(same_color_start) + "+" + std::to_string(total_pixels - 1 - same_color_start) + "_" + last_color;
         }
 
-        // Convert the stringstream to a string
-        std::string s = ss.str();
-
-        // Copy the string to the output buffer and null-terminate it
         std::strncpy(output, s.c_str(), s.size());
         output[s.size()] = '\0';
-
-        // Get the maximum number of bytes and characters for the output buffer
-        size_t max_bytes = sizeof(output);
-        size_t max_chars = max_bytes / sizeof(char) - 1;  // Exclude the null-terminator
     }
+
 }
