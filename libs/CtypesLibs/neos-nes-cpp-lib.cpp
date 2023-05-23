3369e51f647f9b62b9aaa73724b2642a9af53d2a
ikubay <ikubaysan@gmail.com>
2023-05-22 19:46:38 -0700

    Reached 30fps - no need to calculate color char for every pixel

--- libs/CtypesLibs/neos-nes-cpp-lib.cpp
+++ libs/CtypesLibs/neos-nes-cpp-lib.cpp
@@ -55,51 +55,53 @@ extern "C" {
 
         // Iterate over each pixel in the array
         for (int i = 0; i < total_pixels; ++i) {
-            // Access the RGB values of the pixel
-            unsigned char* pixel = array->data + i * array->shape[2];
-            int r = pixel[0] >> 2;
-            int g = pixel[1] >> 2;
-            int b = pixel[2] >> 2;
-
-            // Combine RGB values to obtain an integer representation of the color
-            int rgb_int = b << 10 | g << 5 | r;  // Swap red and blue channels
-
-            // Adjust if in the Unicode surrogate range
-            if (0xD800 <= rgb_int && rgb_int <= 0xDFFF) {
-                if (rgb_int < 0xDC00)
-                    rgb_int = 0xD7FF;  // Maximum value just before the surrogate range
-                else
-                    rgb_int = 0xE000;  // Minimum value just after the surrogate range
-            }
-
-            // Convert the color to a UTF-8 encoded string
-            std::string color = encode_utf8(rgb_int);
-
             // Check if the pixel has changed
             if (changed_pixels == nullptr)
                 changed = true;
             else
                 changed = changed_pixels->data[i];
 
-            //if (changed) passed_in_change_count++;
+            if (changed) {
+                // passed_in_change_count++;
 
-            // Append range and color information to the stringstream if necessary
-            if (changed && color != last_color) {
-                if (!last_color.empty() && same_color_start != -1) {
+                // Access the RGB values of the pixel
+                unsigned char* pixel = array->data + i * array->shape[2];
+                int r = pixel[0] >> 2;
+                int g = pixel[1] >> 2;
+                int b = pixel[2] >> 2;
+
+                // Combine RGB values to obtain an integer representation of the color
+                int rgb_int = b << 10 | g << 5 | r;  // Swap red and blue channels
+
+                // Adjust if in the Unicode surrogate range
+                if (0xD800 <= rgb_int && rgb_int <= 0xDFFF) {
+                    if (rgb_int < 0xDC00)
+                        rgb_int = 0xD7FF;  // Maximum value just before the surrogate range
+                    else
+                        rgb_int = 0xE000;  // Minimum value just after the surrogate range
+                }
+
+                std::string color = encode_utf8(rgb_int);
+                if (color != last_color)
+                {
+                    if (!last_color.empty() && same_color_start != -1) {
                     ss << same_color_start << "+" << i - 1 - same_color_start << "_" << last_color;
                     //range_count++;
                     //std::cout << "Found Range: " << same_color_start << "+" << i - 1 - same_color_start << "_" << last_color << std::endl;
+                    }
+                    same_color_start = i;
+                    last_color = color;
+                    //change_count++;
                 }
-                same_color_start = i;
-                last_color = color;
-                //change_count++;
+         
             } else if (!changed && same_color_start != -1) {
                 ss << same_color_start << "+" << i - 1 - same_color_start << "_" << last_color;
                 //range_count++;
-                //std::cout << "Found Range: " << same_color_start << "+" << i - 1 - same_color_start << "_" << last_color << std::endl;
+                //std::cout << "Found interesting Range: " << same_color_start << "+" << i - 1 - same_color_start << "_" << last_color << std::endl;
                 same_color_start = -1;
                 last_color = "";
             }
+
         }
 
         // After all pixels have been processed, handle any remaining color streak
